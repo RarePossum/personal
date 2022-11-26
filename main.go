98e95e8ece19dffc9b591bf6ec398e4b03c55634
David Ji <david.ji.8484@gmail.com>
2022-11-26 15:37:40 +1100

    test eror fix

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ type Welcome struct {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", DoHealthCheck).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
-  }
-  func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
+	http.ListenAndServe(":8080", router)
+}
+ 
+func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, i'm a golang microservice")
 	w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
-  }
+}
